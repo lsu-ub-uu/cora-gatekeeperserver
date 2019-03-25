1b2ea28a6844cec1c678b7f5daec88edd3ddef48
olovm <olov.mckie@ub.uu.se>
2019-03-25 10:24:21 +0000

    updating to 1.0 for module change

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module se.uu.ub.cora.gatekeeperserver {
 	requires transitive java.ws.rs;
-	requires transitive json;
 	requires transitive javax.servlet.api;
-	requires transitive bookkeeper;
 	requires transitive java.activation;
+	requires transitive se.uu.ub.cora.json;
+	requires transitive se.uu.ub.cora.bookkeeper;
 
 	requires se.uu.ub.cora.gatekeeper;
 
