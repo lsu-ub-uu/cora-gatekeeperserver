24ed474bf2a75d1b0709952940b1a39f67496183
maddekenn <madeleine.kennback@ub.uu.se>
2019-06-25 13:33:54 +0000

    Changing bookkeeper data to cora data

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,12 @@ module se.uu.ub.cora.gatekeeperserver {
 	requires transitive java.ws.rs;
 	requires transitive java.activation;
 	requires transitive se.uu.ub.cora.json;
-	requires transitive se.uu.ub.cora.bookkeeper;
+	// requires transitive se.uu.ub.cora.bookkeeper;
 	requires transitive javax.servlet.api;
 
 	requires se.uu.ub.cora.gatekeeper;
 	requires se.uu.ub.cora.logger;
+	requires se.uu.ub.cora.data;
 
 	uses se.uu.ub.cora.gatekeeper.user.UserPickerProvider;
 	uses se.uu.ub.cora.gatekeeper.user.UserStorageProvider;
