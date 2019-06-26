410354db6aaec2557e95dc77b64bfc413e8aabef
madde <madde@054805b3d4fa>
2019-06-26 10:15:17 +0000

    Removed bookkeeper

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module se.uu.ub.cora.gatekeeperserver {
 	requires transitive java.ws.rs;
 	requires transitive java.activation;
 	requires transitive se.uu.ub.cora.json;
-	// requires transitive se.uu.ub.cora.bookkeeper;
 	requires transitive javax.servlet.api;
 
 	requires se.uu.ub.cora.gatekeeper;
