b3e2e867871771da57b2a434da3bfc4e3c6b6b8b
olovm <olov.mckie@ub.uu.se>
2019-10-30 12:38:10 +0000

    updated dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module se.uu.ub.cora.gatekeeperserver {
 	requires transitive java.ws.rs;
-	requires transitive java.activation;
+	requires transitive jakarta.activation;
 	requires transitive se.uu.ub.cora.json;
 	requires transitive javax.servlet.api;
 
